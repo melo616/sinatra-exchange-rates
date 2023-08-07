85246f7f0b0d62dca99d1e55f1971295420356d8
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-08-07 22:22:51 +0000

    first commit

--- app.rb
+++ app.rb
@@ -1,9 +1,35 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  url = "https://api.exchangerate.host/symbols"
+  api_data = HTTP.get(url)
+  parsed_data = JSON.parse(api_data)
+  @symbols_hash = parsed_data.fetch("symbols")
+  erb(:homepage)
+end
+
+get("/:curr_sym") do
+  @sym = params.fetch("curr_sym")
+  url = "https://api.exchangerate.host/symbols"
+  api_data = HTTP.get(url)
+  parsed_data = JSON.parse(api_data)
+  @symbols_hash = parsed_data.fetch("symbols")
+  erb(:curr_landing)
+end
+
+get("/:curr_from/:curr_to") do
+  @curr_from = params.fetch("curr_from")
+  @curr_to = params.fetch("curr_to")
+  url = "https://api.exchangerate.host/convert?from=" + @curr_from + "&to=" + @curr_to
+  pp "URL"
+  pp url
+  api_data = HTTP.get(url)
+  parsed_data = JSON.parse(api_data)
+  pp "PARSED DATA"
+  pp parsed_data.fetch("result")
+  @result = parsed_data.fetch("result")
+  erb(:convert_page)
 end
